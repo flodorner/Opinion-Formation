2533cd4d5ed76464cb5bbba426b62ef155e7f51c
davejm95 <dave.m@solnet.ch>
2020-12-10 09:02:07 +0100

    rename results folder holme

--- code/Coevolution/results_holme/pickleprinter.py
+++ code/Coevolution/results_holme/pickleprinter.py
@@ -1,5 +1,6 @@
 import pickle, sys
-#print pickle content: python3 pickleprinter.py somethingsomething.pickle
+#print pickle content: 
+# USAGE shell: $ python3 pickleprinter.py somethingsomething.pickle
 def pickleprinter(filename):
     with open(filename, "rb") as f:
                 x=pickle.load(f)
